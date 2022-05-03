464e295ecb7e387d80450e2e3e3a86c13bef9260
Summer Chen <mp4bj6ruby@gmail.com>
2022-05-03 10:20:54 +0800

    signin

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { Serilizer } from 'src/interceptors/SerializerInterceptor';
 import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 import { UserDto } from './dto/user.dto';
 
@@ -12,5 +13,10 @@ export class AuthController {
   @Post('signup')
   signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
+  }
+
+  @Post('signin')
+  signin(@Body() signinDto: SigninDto) {
+    return this.authService.signin(signinDto);
   }
 }
--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -1,7 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 import { UserService } from './user.service';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -18,5 +20,17 @@ export class AuthService {
       throw new BadRequestException('email in use');
     }
     return this.userService.create(data);
+  }
+
+  async signin({ email, password }: SigninDto) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new BadRequestException('user is not exist');
+    }
+    const isPasswordValid = await argon2.verify(user.password, password);
+    if (!isPasswordValid) {
+      throw new BadRequestException('bad password');
+    }
+    return user;
   }
 }
