a716d9f3bea679955dfdae04c45f6efc6969ff54
Summer Chen <mp4bj6ruby@gmail.com>
2022-05-03 09:48:39 +0800

    product CRUD ok

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,8 +1,19 @@
-import { Product } from '@prisma/client';
+import { IsBoolean, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
-export class CreateProductDto implements Partial<Product> {
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsInt()
   price: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
   active: boolean;
+
+  @IsNotEmpty()
+  @IsInt()
   category_id: number;
 }
--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Exclude } from 'class-transformer';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @Exclude()
+  category_id?: number;
+}
