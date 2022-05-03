7920d3f092f6769ed50b1d0b07641ab10097f617
Summer Chen <mp4bj6ruby@gmail.com>
2022-05-03 15:38:25 +0800

    ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, UserModule, CategoryModule, PrismaModule],
+  imports: [
+    ProductModule,
+    UserModule,
+    CategoryModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://chenruby@localhost:5432/myshop?schema=public',
+          url: configService.get('DATABASE_URL'),
         },
       },
     });
