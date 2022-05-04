3bf013dd6b60d95736246b6327d9521ccfbc6e76
Summer Chen <mp4bj6ruby@gmail.com>
2022-05-04 15:12:27 +0800

    preproduction

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('apidoc', app, document);
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8888);
 }
 bootstrap();
