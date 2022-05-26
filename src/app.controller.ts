c50ebe2eb58efccac32afba39e088064c92ccaa1
Summer Chen <mp4bj6ruby@gmail.com>
2022-05-26 09:53:25 +0800

    app service rm default

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Public } from './user/metas/public.meta';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Public()
-  @Post('seed')
-  seed() {
-    return this.appService.seed();
-  }
 }
